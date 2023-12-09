f116caf583a730bf3f8099617afc8ad57d6b0d28
Christina Hanson <christinahansondesu@gmail.com>
2023-12-09 18:11:26 +1300

    Day 9

---
+++ 2023/day9_p1.py
@@ -0,0 +1,17 @@
+from common import *
+import itertools
+import math
+a = get_input()
+
+def extrapolate(n):
+    if set(n) == {0}:
+        return 0
+    ts = []
+    for i, x in enumerate(n[1:], 1):
+        ts.append(x-n[i-1])
+    return n[-1] + extrapolate(ts)
+
+c = 0
+for b in a:
+    c += extrapolate(b.nums)
+print(c)
