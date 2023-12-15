27b19cd88fe631898b07c626766805024abd5f92
Christina Hanson <christinahansondesu@gmail.com>
2023-12-15 18:35:50 +1300

    parsing: move precedence of = to be tighter than ,

--- 2023/common/parse.py
+++ 2023/common/parse.py
@@ -40,7 +40,7 @@ class Aoc:
         if s := self._section():
             return self.split(s)
 
-        for x in ["|", "=", "->", ";", ",", "-", " "]:
+        for x in ["|", "->", ";", ",", "=", "-", " "]:
             if x in self.body:
                 return self.split(x)
 
