4d45dc5e551441c20955155382e808f1f0e9d86b
Christina Hanson <christinahansondesu@gmail.com>
2024-12-06 19:37:09 +1300

    Improve Grid
    Added `extract` and made `Grid[T]` a `Mapping[Point, T]`. Also switched to using 3.12 generic syntax.

--- 2024/common/grid.py
+++ 2024/common/grid.py
@@ -2,7 +2,8 @@ from __future__ import annotations
 
 import itertools
 import functools
-from typing import TypeVar, Callable, Any, Generic, Optional, Iterable, Iterator, Mapping
+from collections.abc import Mapping, MappingView, KeysView, ValuesView, ItemsView
+from typing import TypeVar, Callable, Any, Generic, Iterable, Iterator, Mapping, Self
 
 
 __all__ = (
@@ -12,8 +13,6 @@ __all__ = (
 )
 
 
-T = TypeVar("T")
-G = TypeVar("G")
 Dir = tuple[int, int]
 Point = tuple[int, int]
 
@@ -38,7 +37,42 @@ def invert(d: Dir) -> Dir:
     return -d[0], -d[1]
 
 
-class Grid(Generic[T]):
+class GridView[T](MappingView):
+    grid: Grid[T]
+
+    def __init__(self, grid: Grid[T]) -> None:
+        self.grid = grid
+
+    def __len__(self) -> int:
+        return len(self.grid)
+
+class GridKeys(GridView, KeysView[Point]):
+    def __contains__(self, p: object) -> bool:
+        return p in self.grid
+
+    def __iter__(self) -> Iterator[Point]:
+        return iter(self.grid)
+
+class GridValues[T](GridView[T], ValuesView[T]):
+    def __contains__(self, p: object) -> bool:
+        return p in self.grid.data
+
+    def __iter__(self) -> Iterator[T]:
+        return iter(self.grid.data)
+
+class GridItems[T](GridView[T], ItemsView[Point, T]):
+    def __contains__(self, p: object) -> bool:
+        match p:
+            case tuple((p, v)):
+                return p in self.grid and self.grid.get_unchecked(p) == v  # type: ignore
+            case _:
+                return False
+
+    def __iter__(self) -> Iterator[tuple[Point, T]]:
+        return zip(self.grid, self.grid.data)
+
+
+class Grid[T](Mapping[Point, T]):
     data: list[T]
 
     def __init__(self, w: int, h: int, l: T | Callable[[int, int], T] | list[T]):
@@ -49,7 +83,7 @@ class Grid(Generic[T]):
                 raise ValueError(f"{w}*{h} = {w*h} but only {len(l)} elements provided")
             self.data = l
         elif callable(l):
-            self.data = [l(x, y) for y in range(h) for x in range(w)]
+            self.data = [l(x, y) for y in range(h) for x in range(w)]  # type: ignore
         else:
             self.data = [l for _ in range(w*h)]
 
@@ -66,13 +100,17 @@ class Grid(Generic[T]):
     def __len__(self) -> int:
         return self.width*self.height
 
-    def __contains__(self, p: Point) -> bool:
-        return 0 <= p[0] < self.width and 0 <= p[1] < self.height
+    def __contains__(self, p: object) -> bool:
+        match p:
+            case tuple((int(x), int(y))):
+                return 0 <= p[0] < self.width and 0 <= p[1] < self.height
+            case _:
+                return False
 
     def get_unchecked(self, p: Point) -> T:
         return self.data[p[1]*self.width+p[0]]
 
-    def __getitem__(self, p: Point) -> Optional[T]:
+    def __getitem__(self, p: Point) -> T | None:
         if p not in self:
             return None
         return self.get_unchecked(p)
@@ -124,10 +162,23 @@ class Grid(Generic[T]):
             l.append(self[p])
         return l
 
-    def values(self) -> list[T]:
-        return self.take(self)
+    def extract(self, what: T, repl: T) -> Point:
+        for p, v in self.items():
+            if v == what:
+                self[p] = repl
+                return p
+        raise ValueError(f"{what!r} not found in grid")
 
-    def map(self, f: Callable[[T], G]) -> Grid[G]:
+    def keys(self) -> GridKeys:
+        return GridKeys(self)
+
+    def values(self) -> GridValues[T]:
+        return GridValues(self)
+
+    def items(self) -> GridItems[T]:
+        return GridItems(self)
+
+    def map[G](self, f: Callable[[T], G]) -> Grid[G]:
         for i, x in enumerate(self.data):
             self.data[i] = f(x)  # type: ignore
         return self  # type: ignore
@@ -141,10 +192,10 @@ class Grid(Generic[T]):
     def columns(self) -> list[list[Point]]:
         return [[(x, y) for y in range(self.height)] for x in range(self.width)]
 
-    def blit(self, point: Point, other: Grid[T]) -> Grid[T]:
-        for p in other:
+    def blit(self, point: Point, other: Mapping[Point, T]) -> Grid[T]:
+        for p, v in other.items():
             try:
-                self[offset(p, point)] = other.get_unchecked(p)
+                self[offset(p, point)] = v
             except IndexError:
                 pass
         return self
@@ -157,8 +208,8 @@ class Grid(Generic[T]):
         for grid in summands:
             if grid.height != height:
                 raise ValueError("concat arguments differ in height")
-            for x, y in grid:
-                r[x+o, y] = grid.get_unchecked((x, y))
+            for (x, y), v in grid.items():
+                r[x+o, y] = v
             o += grid.width
         return r
 
@@ -170,8 +221,8 @@ class Grid(Generic[T]):
         for grid in summands:
             if grid.width != width:
                 raise ValueError("vconcat arguments differ in width")
-            for x, y in grid:
-                r[x, y+o] = grid.get_unchecked((x, y))
+            for (x, y), v in grid.items():
+                r[x, y+o] = v
             o += grid.height
         return r
 
@@ -202,7 +253,7 @@ class Grid(Generic[T]):
         return Grid(w, h, lambda x, y: self.get_unchecked((w-y-1, x)))
 
     @classmethod
-    def parse(cls, s: str) -> Grid[str]:
+    def parse(cls: type[Grid], s: str) -> Grid[str]:
         return cls.from_list(s.splitlines())
 
     @classmethod
@@ -231,7 +282,7 @@ class Grid(Generic[T]):
     @classmethod
     def from_dict(cls, d: Mapping[Point, T]) -> Grid[T]:
         if not d:
-            return cls(0, 0, None)
+            return cls(0, 0, [])
         min_x = float("inf")
         max_x = float("-inf")
         min_y = float("inf")
@@ -246,11 +297,11 @@ class Grid(Generic[T]):
     def tile(self) -> SparseGrid[T]:
         return SparseGrid(lambda p: self.get_unchecked((p[0] % self.width, p[1] % self.height)))
 
-    def isolate(self, using: G) -> SparseGrid[T | G]:
-        return SparseGrid(lambda _: using, {p: self.get_unchecked(p) for p in self})
+    def isolate[G](self, using: G) -> SparseGrid[T | G]:
+        return SparseGrid(lambda _: using, dict(self))
 
 
-class SparseGrid(Generic[T], dict[Point, T]):
+class SparseGrid[T](dict[Point, T]):
     def __init__(self, f: Callable[[Point], T], *args, **kwargs):
         self.f = f
         super().__init__(*args, **kwargs)
