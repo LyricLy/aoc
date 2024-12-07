efc5e84bcab67eb8456f9a42606cc4056c4a7913
Christina Hanson <christinahansondesu@gmail.com>
2024-12-07 18:34:49 +1300

    Day 7

---
+++ 2024/day07_p1.py
@@ -0,0 +1,16 @@
+from common import *
+a = get_input()
+
+def can_do_it(target, nums):
+    assert len(nums) > 1
+    frontier = {nums[0]}
+    for num in nums[1:]:
+        frontier = {x + num for x in frontier} | {x * num for x in frontier}
+    return target in frontier
+
+from tqdm import tqdm
+c = 0
+for b in tqdm(a):
+    if can_do_it(b.id, b.nums):
+        c += b.id
+print(c)
